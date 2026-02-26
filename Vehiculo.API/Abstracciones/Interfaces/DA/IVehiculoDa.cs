3eebdaad90886b9156fc72a13a9ff4be78e4d079
G4b00 <gchavesrojas@gmail.com>
2026-02-25 19:00:18 -0600

    first commit

---
+++ Vehiculo.API/Abstracciones/Interfaces/DA/IVehiculoDa.cs
@@ -0,0 +1,13 @@
+﻿using Abstracciones.Modelos;
+
+namespace Abstracciones.Interfaces.DA
+{
+    public interface IVehiculoDa
+    {
+        Task<IEnumerable<VehiculoResponse>> Obtener();
+        Task<VehiculoResponse> Obtener(Guid Id);
+        Task<Guid> Agregar(VehiculoRequest vehiculo);
+        Task<Guid> Editar(Guid Id, VehiculoRequest vehiculo);
+        Task<Guid> Eliminar(Guid Id);
+    }
+}
---
+++ Vehiculo.API/Flujo/VehiculoFlujo.cs
@@ -0,0 +1,46 @@
+﻿using Abstracciones.Interfaces.DA;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flujo
+{
+    public class VehiculoFlujo : IVehiculoFlujo
+    {
+        private IVehiculoDa _vehiculoDa;
+
+        public VehiculoFlujo(IVehiculoDa vehiculoDa)
+        {
+            _vehiculoDa = vehiculoDa;
+        }
+
+        public Task<Guid> Agregar(VehiculoRequest vehiculo)
+        {
+            return _vehiculoDa.Agregar(vehiculo);
+        }
+
+        public Task<Guid> Editar(Guid Id, VehiculoRequest vehiculo)
+        {
+            return _vehiculoDa.Editar(Id, vehiculo);
+        }
+
+        public Task<Guid> Eliminar(Guid Id)
+        {
+            return _vehiculoDa.Eliminar(Id);
+        }
+
+        public Task<IEnumerable<VehiculoResponse>> Obtener()
+        {
+            return _vehiculoDa.Obtener();
+        }
+
+        public Task<VehiculoResponse> Obtener(Guid Id)
+        {
+            return _vehiculoDa.Obtener(Id);
+        }
+    }
+}
